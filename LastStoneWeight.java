527edfa59f16904a2ec220c791b0981904d21e38
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-04-24 18:56:00 +0530

    Create LastStoneWeight.java

---
+++ LastStoneWeight.java
@@ -0,0 +1,26 @@
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+If x == y, both stones are destroyed, and
+If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+At the end of the game, there is at most one stone left.
+
+Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+ SOLUTION:
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+        while (maxHeap.size() != 1) {
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+            maxHeap.offer(Math.abs(stone1 - stone2));
+        }
+        return maxHeap.poll();
+    }
+}
