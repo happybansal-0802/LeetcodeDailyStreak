bcf459a786f81b8b2e6b7571fcf8db365ef91bad
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-04-28 09:01:00 +0530

    Create Similar String Groups.java

---
+++ Similar String Groups.java
@@ -0,0 +1,93 @@
+Two strings X and Y are similar if we can swap two letters (in different positions) of X, so that it equals Y. Also two strings X and Y are similar if they are equal.
+
+For example, "tars" and "rats" are similar (swapping at positions 0 and 2), and "rats" and "arts" are similar, but "star" is not similar to "tars", "rats", or "arts".
+
+Together, these form two connected groups by similarity: {"tars", "rats", "arts"} and {"star"}.  Notice that "tars" and "arts" are in the same group even though they are not similar.  Formally, each group is such that a word is in the group if and only if it is similar to at least one other word in the group.
+
+We are given a list strs of strings where every string in strs is an anagram of every other string in strs. How many groups are there?
+  SOLUTION:
+class Solution {
+    class UnionFind {
+        int[] parent, rank;
+        int count;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            count = n;
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) {
+                return false;
+            }
+
+            if (rank[rootX] < rank[rootY]) {
+                int temp = rootX;
+                rootX = rootY;
+                rootY = temp;
+            }
+
+            parent[rootY] = rootX;
+
+            if (rank[rootX] == rank[rootY]) {
+                rank[rootX]++;
+            }
+
+            count--;
+
+            return true;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+
+    private boolean isSimilar(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+
+        return diff == 2;
+    }
+}
