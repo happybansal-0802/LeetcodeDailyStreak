b82025e9407fab09240c883876e3a09226d95e39
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-05-06 22:16:55 +0530

    Create  Number of Subsequences That Satisfy the Given Sum Condition.java

---
+++ Number of Subsequences That Satisfy the Given Sum Condition.java
@@ -0,0 +1,22 @@
+You are given an array of integers nums and an integer target.
+
+Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+  Solution:
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = 0, n = nums.length, l = 0, r = n - 1, mod = (int)1e9 + 7;
+        int[] pows = new int[n];
+        pows[0] = 1;
+        for (int i = 1 ; i < n ; ++i)
+            pows[i] = pows[i - 1] * 2 % mod;
+        while (l <= r) {
+            if (nums[l] + nums[r] > target) {
+                r--;
+            } else {
+                res = (res + pows[r - l++]) % mod;
+            }
+        }
+        return res;
+    }
+}
