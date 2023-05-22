3ac48343fb8f145778ff9c7e857bc33b93776dc4
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-05-22 15:09:39 +0530

    Create Top K frequent Elements.java

---
+++ Top K frequent Elements.java
@@ -0,0 +1,22 @@
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+  Solution:
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> counter.get(b) - counter.get(a));
+        for (int num : counter.keySet()) {
+            heap.offer(num);
+        }
+        
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = heap.poll();
+        }
+        
+        return result;
+    }
+}
