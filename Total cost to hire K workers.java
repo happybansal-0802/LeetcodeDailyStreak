ba029e4d9d298f7928d89e62f84f9ad3ec0ec193
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-06-26 23:19:20 +0530

    Create Total cost to hire K workers.java

---
+++ Total cost to hire K workers.java
@@ -0,0 +1,42 @@
+You are given a 0-indexed integer array costs where costs[i] is the cost of hiring the ith worker.
+
+You are also given two integers k and candidates. We want to hire exactly k workers according to the following rules:
+
+You will run k sessions and hire exactly one worker in each session.
+In each hiring session, choose the worker with the lowest cost from either the first candidates workers or the last candidates workers. Break the tie by the smallest index.
+For example, if costs = [3,2,7,7,1,2] and candidates = 2, then in the first hiring session, we will choose the 4th worker because they have the lowest cost [3,2,7,7,1,2].
+In the second hiring session, we will choose 1st worker because they have the same lowest cost as 4th worker but they have the smallest index [3,2,7,7,2]. Please note that the indexing may be changed in the process.
+If there are fewer than candidates workers remaining, choose the worker with the lowest cost among them. Break the tie by the smallest index.
+A worker can only be chosen once.
+Return the total cost to hire exactly k workers.
+  Solution:
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+        int i = 0;
+        int j = costs.length - 1;
+        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+
+        long ans = 0;
+        while (k-- > 0) {
+            while (pq1.size() < candidates && i <= j) {
+                pq1.offer(costs[i++]);
+            }
+            while (pq2.size() < candidates && i <= j) {
+                pq2.offer(costs[j--]);
+            }
+
+            int t1 = pq1.size() > 0 ? pq1.peek() : Integer.MAX_VALUE;
+            int t2 = pq2.size() > 0 ? pq2.peek() : Integer.MAX_VALUE;
+
+            if (t1 <= t2) {
+                ans += t1;
+                pq1.poll();
+            } else {
+                ans += t2;
+                pq2.poll();
+            }
+        }
+        return ans;
+    }
+}
