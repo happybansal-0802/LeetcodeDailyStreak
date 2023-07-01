89116df8a058b3d0700cc99a3d6ee4499a90cf45
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-07-01 16:03:17 +0530

    Create Fair Distribution of cookies.java

---
+++ Fair Distribution of cookies.java
@@ -0,0 +1,29 @@
+You are given an integer array cookies, where cookies[i] denotes the number of cookies in the ith bag. You are also given an integer k that denotes the number of children to distribute all the bags of cookies to. All the cookies in the same bag must go to the same child and cannot be split up.
+
+The unfairness of a distribution is defined as the maximum total cookies obtained by a single child in the distribution.
+
+Return the minimum unfairness of all distributions.
+  Solution:
+class Solution {
+    int ans = Integer.MAX_VALUE;
+
+    void helper(int[] cookies, int start, int k, int[] temp) {
+        if (start == cookies.length) {
+            int max = 0;
+            for (int c : temp)
+                max = Math.max(max, c);
+            ans = Math.min(ans, max);
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            temp[i] += cookies[start];
+            helper(cookies, start + 1, k, temp);
+            temp[i] -= cookies[start];
+        }
+    }
+
+    public int distributeCookies(int[] cookies, int k) {
+        helper(cookies, 0, k, new int[k]);
+        return ans;
+    }
+}
