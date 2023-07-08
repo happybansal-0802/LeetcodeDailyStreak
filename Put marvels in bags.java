bb22ddb7c8aa97683df55a3ae8b2b4b3be2afba8
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-07-08 19:59:25 +0530

    Create Put marvels in bags.java

---
+++ Put marvels in bags.java
@@ -0,0 +1,28 @@
+You have k bags. You are given a 0-indexed integer array weights where weights[i] is the weight of the ith marble. You are also given the integer k.
+
+Divide the marbles into the k bags according to the following rules:
+
+No bag is empty.
+If the ith marble and jth marble are in a bag, then all marbles with an index between the ith and jth indices should also be in that same bag.
+If a bag consists of all the marbles with an index from i to j inclusively, then the cost of the bag is weights[i] + weights[j].
+The score after distributing the marbles is the sum of the costs of all the k bags.
+
+Return the difference between the maximum and minimum scores among marble distributions.
+  Solution:
+  class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        int[] pairs = new int[n - 1];
+        for (int i = 1; i < n; i++) {
+            pairs[i - 1] = weights[i] + weights[i - 1];
+        }
+        Arrays.sort(pairs);
+        long minScore = 0;
+        long maxScore = 0;
+        for (int i = 0; i < k - 1; i++) {
+            minScore += pairs[i];
+            maxScore += pairs[n - i - 2];
+        }
+        return maxScore - minScore;
+    }
+}
