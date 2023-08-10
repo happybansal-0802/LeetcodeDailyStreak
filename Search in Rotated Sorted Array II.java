1c8766e78baef1561b73205229153f4f12d34771
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-08-10 22:17:42 +0530

    Create Search in Rotated Sorted Array II.java

---
+++ Search in Rotated Sorted Array II.java
@@ -0,0 +1,38 @@
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+
+  Solution:
+  class Solution {
+    public boolean search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] == nums[i] && nums[mid] == nums[j]) {
+                i = i + 1;
+                j = j - 1;
+            } else if (nums[mid] >= nums[i]) {
+                if (target >= nums[i] && target < nums[mid]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            } else if (nums[mid] <= nums[j]) {
+                if (target > nums[mid] && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
