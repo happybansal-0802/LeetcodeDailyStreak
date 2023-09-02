42887e2f4dc75898e4b79d2f97ac6fcc581a396b
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-09-02 19:34:59 +0530

    Create Extra characters in a String.java

---
+++ Extra characters in a String.java
@@ -0,0 +1,25 @@
+You are given a 0-indexed string s and a dictionary of words dictionary. You have to break s into one or more non-overlapping substrings such that each substring is present in dictionary. There may be some extra characters in s which are not present in any of the substrings.
+
+Return the minimum number of extra characters left over if you break up s optimally.
+
+ Solution:
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        int maxVal = s.length() + 1;
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, maxVal);
+        dp[0] = 0;
+
+        Set<String> dictionarySet = new HashSet<>(Arrays.asList(dictionary));
+
+        for (int i = 1; i <= s.length(); ++i) {
+            dp[i] = dp[i - 1] + 1;
+            for (int l = 1; l <= i; ++l) {
+                if (dictionarySet.contains(s.substring(i - l, i))) {
+                    dp[i] = Math.min(dp[i], dp[i - l]);
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
