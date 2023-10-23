d3fddf3a8b4974d6dd3ff73fccc2931bab5c180d
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-10-23 22:08:57 +0530

    Create Power of four.java

---
+++ Power of four.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+         if (n <= 0) {
+            return false;
+        }
+        return Math.log(n) / Math.log(4) % 1 == 0;     
+    }
+}
