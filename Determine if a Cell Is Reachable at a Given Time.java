9916dd59014d60d44d4fe9bc9592f8c2ad7c5bd4
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-11-08 22:41:00 +0530

    Create Determine if a Cell Is Reachable at a Given Time.java

---
+++ Determine if a Cell Is Reachable at a Given Time.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int xdiff = Math.abs(sx - fx);
+        int ydiff = Math.abs(sy - fy);
+        
+        if (xdiff == 0 && ydiff == 0 && t == 1) {
+            return false;
+        }
+        
+        return (Math.min(xdiff, ydiff) + Math.abs(xdiff - ydiff)) <= t;
+    }
+
+   
+}
+
